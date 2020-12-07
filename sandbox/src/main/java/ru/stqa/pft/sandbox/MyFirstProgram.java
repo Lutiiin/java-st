7ec270b94ebab641a808308223598839f7bebb46
Алена Егупова <4erten@gmail.com>
2020-12-08 01:04:16 +0300

    Добавлены классы вычисления площади квадрата и прямоугольника

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,20 @@
 package ru.stqa.pft.sandbox;
 
 class MyFirstProgram {
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        hello("world");
+        hello("user");
+        hello("Alena");
+
+        Sqare s = new Sqare(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(2, 5);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    }
+
+    public static void hello (String somebody){
+        System.out.println("Hello, " + somebody + "!");
     }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Sqare.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+public class Sqare {
+
+    double l;
+
+    public Sqare(double l){
+        this.l = l;
+    }
+
+    public double area(){
+        return this.l * this.l;
+    }
+}
