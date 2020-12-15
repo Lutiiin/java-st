75adf458b279a18c48b0dacaf582fb7393a299ee
Алена Егупова <4erten@gmail.com>
2020-12-15 21:54:18 +0300

    Добавлен и настроен класс HelperBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -3,24 +3,19 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class SessionHelper {
-    FirefoxDriver wd;
+public class SessionHelper extends HelperBase{
 
     public SessionHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void login(String username, String pass) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(pass);
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
+        type(By.name("user"), username);
+        type(By.name("pass"), pass);
+        click(By.xpath("//input[@value='Login']"));
     }
 
     public void logout() {
-        wd.findElement(By.linkText("Logout")).click();
+        click(By.linkText("Logout"));
     }
 }
