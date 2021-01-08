c530c72d9d447a5509dd7c02cfe73602ce4164dd
Lutiiin <4ertenock@mail.ru>
2021-01-08 23:58:17 +0300

    Реализована проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
 
-public class GroupDeleteTests extends TestBase {
+public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDelete() throws Exception {
     app.getNavigationHelper().goToGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
