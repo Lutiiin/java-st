647a697e41b2564509671ffaebc7d36caa78a1b8
Lutiiin <4ertenock@mail.ru>
2021-01-09 19:04:38 +0300

    Добавлены проверки корректности добавления/удаления групп/контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,13 +1,21 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.List;
 
 public class ContactCreationTests extends TestBase{
 
   @Test
   public void testContactCreation() throws Exception {
+    app.getNavigationHelper().goToHomePage();
+    List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().createContact(new ContactData("Ivan", "Sidorov", "test@test.ru", "Moscow", "+74950982376"));
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() + 1);
     app.getSessionHelper().logout();
   }
 }
