daf880b40af5f2bb2f412223a806f92c019ace8d
Алена Егупова <4erten@gmail.com>
2021-01-20 13:05:34 +0300

    Вынесено описание тестового стенда в конфигурационный файл

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,9 +6,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+    private Properties properties;
     WebDriver wd;
 
     private SessionHelper sessionHelper;
@@ -17,8 +22,11 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     private String browser;
 
-    public ApplicationManager(String browser) {
+    public ApplicationManager(String browser) throws IOException {
         this.browser = browser;
+        String target = System.getProperty("target", "local");
+        Properties properties = new Properties();
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
     }
 
     public void init() {
@@ -32,12 +40,12 @@ public class ApplicationManager {
             wd = new OperaDriver();
         }
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook");
+        wd.get(properties.getProperty("web.baseUrl"));
         sessionHelper = new SessionHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         groupHelper = new GroupHelper(wd);
         contactHelper = new ContactHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.loginAdmin"), properties.getProperty("web.passwordAdmin"));
     }
 
     public void stop() {
