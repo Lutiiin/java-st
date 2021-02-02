92f7375fa7bcced3efdd768aa15e518bbe2100c4
Алена Егупова <4erten@gmail.com>
2021-02-02 13:01:17 +0300

    Обновлен тест для добавления контакта в группу

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactInGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactInGroupTests.java
@@ -31,17 +31,23 @@ public class AddContactInGroupTests extends TestBase{
   @Test
   public void testAddContactInGroup() throws Exception {
     String id =  String.valueOf(app.db().groups().iterator().next().getId());
+    Contacts contactsNotInGroup = new Contacts();
     Contacts before = app.db().contactsInGroup(id);
-    Contacts contactsNotInGroup = app.db().contacts().withoutContactInGroup(before);
-    if (contactsNotInGroup.size() != 0){
+    contactsNotInGroup = app.db().contacts().withoutContactInGroup(before);
+    if (contactsNotInGroup.size() == 0){
+      app.goTo().homePage();
+      app.contact().create(new ContactData()
+              .withFirstName("Ivan").withLastName("Sidorov")
+              .withHomePhone("+74956734512").withMobilePhone("+(789)92396534").withWorkPhone("+7499-452-09-45")
+              .withEmail("any_one@test.com").withEmail2("any_two@test.com").withEmail3("any_two@test.com")
+              .withAddress("Какой-то адрес раз"));
+      contactsNotInGroup = app.db().contacts().withoutContactInGroup(before);
+    }
       ContactData addedContact = contactsNotInGroup.iterator().next();
       app.goTo().homePage();
       app.contact().addToGroup(addedContact, id);
       assertThat(app.contact().countContactsInGroup(id), equalTo(before.size() + 1));
       Contacts after = app.db().contactsInGroup(id);
       assertThat(after, equalTo(before.withAdded(addedContact)));
-    } else {
-      System.out.println("Все контакты добавлены в группу!");
-    }
   }
 }
